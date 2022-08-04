5daa0a89a9d51ef97daab1d13decc3577ba17ab5
Samuel Lihn <samuellihn@gmail.com>
2022-08-04 15:25:20 -0400

    added about and faq

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,23 @@ import Button from "../components/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faClock, faCalendar} from "@fortawesome/free-regular-svg-icons";
 import {faMapLocationDot} from "@fortawesome/free-solid-svg-icons";
-import Expander from "../components/Expander";
+import SponsorsSection from "../sections/sponsors";
+import FaqSection from "../sections/faq";
+import AboutSection from "../sections/aboutsection";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <div className={styles["site"]}>
+            <Head>
+                <title>HackMCA Hackathon</title>
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta property="og:title" content="HackMCA Hackathon"/>
+                <meta property="og:image" content="https://hackmca.info/hackmca-logo-ogpreview.png"/>
+                <meta property="og:image:width" content="1920px"/>
+                <meta property="og:image:height" content="1080px"/>
+            </Head>
             <div className={styles["home-screen"]}>
                 <div className={styles["home-screen-inner"]}>
                     <div className={styles["centered-v"]}>
@@ -19,7 +31,7 @@ export default function Home() {
                     </div>
                     <div className={styles["fancy-list"]}>
                         <span><FontAwesomeIcon icon={faCalendar}/>October 1st, 2022</span>
-                        <span><FontAwesomeIcon icon={faClock}/>8 am - 8 pm</span>
+                        <span><FontAwesomeIcon icon={faClock}/>8 am - 9 pm</span>
                         <span><FontAwesomeIcon icon={faMapLocationDot}/>Middlesex County College</span>
                     </div>
 
@@ -34,7 +46,7 @@ export default function Home() {
                             Join us for a day of coding, inventing, and learning on October 1, 2022! </p>
                         <div className={styles["fancy-list"]}>
                             <span><FontAwesomeIcon icon={faCalendar}/><b>Date:</b> October 1st, 2022</span>
-                            <span><FontAwesomeIcon icon={faClock}/><b>Time:</b> 8 am - 8 pm</span>
+                            <span><FontAwesomeIcon icon={faClock}/><b>Time:</b> 8 am - 9 pm</span>
                             <span><FontAwesomeIcon
                                 icon={faMapLocationDot}/><b>Venue:</b> Middlesex County College</span>
                         </div>
@@ -42,40 +54,9 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className={[styles["content-section-light"], styles["content-section"]].join(" ")}>
-                <div className={styles["content-section-inner"]}>
-                    <h1>Sponsors</h1>
-                    <p>We are looking for sponsors for this event! If you or your organization would like to sponsor
-                        this event and reach hundreds of motivated, tech-minded high school students, please reach out
-                        to <a href={"mailto:ravishankars@mcmsnj.net"}>ravishankars@mcmsnj.net</a></p>
-                </div>
-            </div>
-            <div className={[styles["content-section-yellow"], styles["content-section"]].join(" ")}>
-                <div className={styles["content-section-inner"]}>
-                    <h1>Frequently Asked Questions</h1>
-                    <div className={styles["expander-group"]}>
-                        <Expander titleText={"What is a hackathon?"}>A hackathon</Expander>
-                        <Expander titleText={"Who can join?"}>A hackathon</Expander>
-                        <Expander titleText={"What do I need to bring?"}>A hackathon</Expander>
-                        <Expander titleText={"What if I don't know how to code?"}>A hackathon</Expander>
-                        <Expander titleText={"What if I have another question?"}>A hackathon</Expander>
-                        <hr/>
-                    </div>
-                </div>
-            </div>
-            <div className={[styles["content-section-light"], styles["content-section"]].join(" ")}>
-                <div className={styles["content-section-inner"]}>
-                    <h1>About Us</h1>
-                    <p><b>We are the Middlesex County Academy Computer Science Club.</b> We're a diverse group of
-                        students from the Middlesex County Academy in Edison, New Jersey who are passionate about
-                        computer science and technology. The club has competed in the American Computer Science League,
-                        and recently we've branched into hosting events such as the Middlesex County Academy Informatics
-                        Competition. Now we're hosting our first ever hackathon, and we're just as excited about it as
-                        you are!</p>
-                    <h2>Our Team</h2>
-
-                </div>
-            </div>
+            <SponsorsSection/>
+            <FaqSection/>
+            <AboutSection/>
         </div>
     )
 }
